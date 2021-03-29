70d979501eb09b16a1d31df79503e3f9a39d5fcd
Genesis <mr_felix_becker@web.de>
2021-03-29 18:56:14 +0200

    Fixed Merge

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -1,4 +1,3 @@
-<<<<<<< Updated upstream:Assets/Scripts/CameraMovement.cs
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -31,37 +30,3 @@ public class CameraMovement : MonoBehaviour
         transform.position += movement;
     }
 }
-=======
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class CameraMovement : MonoBehaviour
-{
-
-    [SerializeField] float movementSpeed = 1f;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Cursor.lockState = CursorLockMode.Confined;
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        Vector3 movement = new Vector3();
-        if (Input.GetKey(KeyCode.W)) movement.z += movementSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.A)) movement.x -= movementSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.S)) movement.z -= movementSpeed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.D)) movement.x += movementSpeed * Time.deltaTime;
-
-        if (Input.mousePosition.x <= 1) movement.x -= movementSpeed * Time.deltaTime;
-        if (Input.mousePosition.x >= Screen.width - 1) movement.x += movementSpeed * Time.deltaTime;
-        if (Input.mousePosition.y <= 1) movement.z -= movementSpeed * Time.deltaTime; ;
-        if (Input.mousePosition.y >= Screen.height - 1) movement.z += movementSpeed * Time.deltaTime; ;
-
-
-        transform.position += movement;
-    }
-}
->>>>>>> Stashed changes:Assets/CameraMovement.cs
